dc6b6b1c18a8e0bb95d8097e2697e4cd09c06eb4
certifiedTboy <etosin70@gmail.com>
2025-03-18 12:21:57 +0100

    update app

--- src/components/inbox/messages/MessageList.tsx
+++ src/components/inbox/messages/MessageList.tsx
@@ -4,7 +4,7 @@ import styles from "./Messges.module.css";
 const MessageList = () => {
   return (
     <div
-      className={`w-[25%] ${styles.message_list_container} hidden sm:block relative`}
+      className={`${styles.message_list_container} hidden sm:block relative`}
     >
       <div className="sticky top-25">
         <SearchMessageInput />
